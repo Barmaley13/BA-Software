2ee5b9ee61b2f98ba508de9eaaffefbf21309d8e
Barmaley13 <kbelyayev@gmail.com>
2017-05-24 18:58:25 -0600

    Updating node_update functionality 3

--- gate/sleepy_mesh/networks/network/executor.py
+++ gate/sleepy_mesh/networks/network/executor.py
@@ -116,10 +116,11 @@ class NetworkExecutor(NetworkBase):
             if update_type == 'node_update':
                 if node is not None and node['type'] != 'base':
                     # Send update request directly to base node
-                    update_args = ['smn__node_update']
+                    raw_net_addr = conversions.hex_to_bin(node['net_addr'])
+                    update_args = ['smn__node_update', raw_net_addr]
                     update_args += self.__update_args(node)
 
-                    self._manager.bridge.network_ucast(node['net_addr'], *update_args)
+                    self._manager.bridge.base_node_ucast(*update_args)
                     LOGGER.debug("Node Update Args: {}".format(update_args))
 
             else:
