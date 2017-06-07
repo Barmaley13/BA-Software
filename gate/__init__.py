89eb7a4d78ca65f71fd869041f708a3ee19e875c
Barmaley13 <kbelyayev@gmail.com>
2017-06-07 17:55:34 -0600

    Improving headers 3

--- gate/__init__.py
+++ gate/__init__.py
@@ -4,5 +4,5 @@ Highly flexible & customizable low power Wireless Mesh Network System
 """
 
 __author__ = 'Kirill V. Belyayev'
-__version__ = '03.05.03'
+__version__ = '03.05.04'
 __license__ = 'GNUv3'
